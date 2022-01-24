554f89179c95e0237d2a4e2e38536ba704834b24
Nina Kampa <ninakmb@hotmail.com>
2022-01-24 15:50:11 +0200

    🚚 Fixes project structure

---
+++ src/MyClientelle.Web/Controllers/PatientController.cs
@@ -0,0 +1,10 @@
+namespace Kampa.MyClientelle.Web.Controllers
+{
+  using Microsoft.AspNetCore.Mvc;
+
+  [ApiController]
+  [Route("api/patient")]
+  public class PatientController : ControllerBase
+  {
+  }
+}
